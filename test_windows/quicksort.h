9c95e5b2defe91eb55ce610e276cb1dc5751435d
dev-acoustikue <acoustikue@gmail.com>
2020-05-20 15:35:14 +0900

    error fix

--- test_windows/quicksort.h
+++
@@ -1,9 +0,0 @@
-#ifndef QUICKSORT_H_
-#define QUICKSORT_H_
-#include <cstddef>
-
-void quicksort(int *arr, std::size_t len);
-int kthSmallestNaive(int *arr, std::size_t len, int k);
-int kthSmallest(int *arr, std::size_t len, int k);
-
-#endif
